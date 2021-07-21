fcf87f6ba258e4491e0ee0032cd32d44a67ff4c7
Valentino-vada <tataurov_qi@yahoo.com>
2021-07-21 21:52:49 +0300

    add file1.json, file1.json, index.js to src/

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as fs from 'fs';
+import _ from 'lodash';
+
+const genDiff = () => {
+    const path = path.resolve();
+    const dir = process.cwd();
+    const filepath = fs.readFileSync();
+
+};
