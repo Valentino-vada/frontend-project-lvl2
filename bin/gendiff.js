7234e62878b1aec96f76d8c16ec410e4e2c66e48
Valentino-vada <tataurov_qi@yahoo.com>
2021-07-21 21:46:20 +0300

    add ation to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander/esm.mjs';
+import { Command }  from 'commander/esm.mjs';
 
 const program = new Command();
 
@@ -9,4 +9,5 @@ program
         .description('Compares two configuration files and shows a difference.')
         .option('-f, --format [type]',  'output format')
         .argument('<filepath1> <filepath2>')
+        .action(filepath1, filepath2)
          program.parse();
