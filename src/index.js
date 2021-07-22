ec5e26859d237ea525f8867b6cdb9fa9fcfc7549
Valentino-vada <tataurov_qi@yahoo.com>
2021-07-22 21:43:49 +0300

    add str to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import * as fs from 'fs';
 import _ from 'lodash';
+import * as fs from 'fs';
+import path from 'path';
 
-const genDiff = () => {
-    const path = path.resolve();
-    const dir = process.cwd();
-    const filepath = fs.readFileSync();
 
+const genDiff = (filepath1, filepath2) => {
+    const pathAbs1 = path.resolve(process.cwd(filepath1));
+    const pathAbs2 = path.resolve(process.cwd(filepath2));
+    const pathAbs3 = process.cwd(filepath1);
+    const pathAbs4 = process.cwd(filepath2);
+    const content1 = fs.readFileSync(filepath1, 'utf8');
+    const content2 = fs.readFileSync(filepath2, 'utf8');
+    
+return console.log({content1, content2});
 };
+
+const a = genDiff();
+console.log(a);
