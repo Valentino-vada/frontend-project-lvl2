8533cfa18d87c6b9685f296b72f87312ee99987b
Valentino-vada <tataurov_qi@yahoo.com>
2021-07-22 21:40:42 +0300

    add str to bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,7 @@ import { Command }  from 'commander/esm.mjs';
 const program = new Command();
 
 program
-        .version('0.0.1')
+        .version('1.0.0')
         .description('Compares two configuration files and shows a difference.')
         .option('-f, --format [type]',  'output format')
         .argument('<filepath1> <filepath2>')
