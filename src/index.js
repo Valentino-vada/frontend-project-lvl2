5575b0358331d4d39f565ed82bfd11b28d6f76d5
Valentino-vada <tataurov_qi@yahoo.com>
2021-07-24 19:53:05 +0300

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import _ from 'lodash';
-import * as fs from 'fs';
+import fs from 'fs';
 import path from 'path';
 
 
 const genDiff = (filepath1, filepath2) => {
-    const pathAbs1 = path.resolve(process.cwd(filepath1));
-    const pathAbs2 = path.resolve(process.cwd(filepath2));
-    const pathAbs3 = process.cwd(filepath1);
-    const pathAbs4 = process.cwd(filepath2);
+    const pathAbs1 = path.resolve(filepath1);
+    const pathAbs2 = path.resolve(filepath2);
     const content1 = fs.readFileSync(filepath1, 'utf8');
     const content2 = fs.readFileSync(filepath2, 'utf8');
-    
-return console.log({content1, content2});
+    let obj = Object.assign(content1, content2);
+return console.log(obj);
 };
-
-const a = genDiff();
-console.log(a);
+// 'src/file1.json', 'src./file2.json'
+const a = genDiff('src/file1.json', 'src/file2.json');
+export default genDiff;
