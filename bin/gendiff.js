7b04a82e7d492af6cdb0056f0b643426cfbf4094
Valentino-vada <tataurov_qi@yahoo.com>
2021-07-29 22:28:06 +0300

    fix to src/index.js and bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,11 +4,11 @@ import program from 'commander';
 import genDiff from '../src/index.js';
 
 program
-        .version('1.0.0')
-        .description('Compares two configuration files and shows a difference.')
-        .option('-f, --format [type]',  'output format')
-        .argument('<filepath1> <filepath2>')
-        .action((filepath1, filepath2) => {
-                console.log(genDiff(filepath1, filepath2));
-        });
-         program.parse();
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
+program.parse();
--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 
-
 const genDiff = (filepath1, filepath2) => {
-    //const pathAbs1 = path.resolve(filepath1);
-    //const pathAbs2 = path.resolve(filepath2);
-    const content1 = fs.readFileSync(filepath1, 'utf8');
-    const content2 = fs.readFileSync(filepath2, 'utf8');
-    const obj = { ...content1, ...content2 };
-return console.log(obj);
+  // const pathAbs1 = path.resolve(filepath1);
+  // const pathAbs2 = path.resolve(filepath2);
+  const content1 = fs.readFileSync(filepath1, 'utf8');
+  const content2 = fs.readFileSync(filepath2, 'utf8');
+  const obj = { ...content1, ...content2 };
+  return console.log(obj);
 };
 // 'src/file1.json', 'src./file2.json'
 const a = genDiff('file1.json', 'file2.json');
