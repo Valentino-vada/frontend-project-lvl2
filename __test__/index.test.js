308d1453772be16c6bc50f11f40d976e62f6ffd0
Valentino-vada <tataurov_qi@yahoo.com>
2021-08-21 11:59:48 +0300

    added test .yml

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -8,8 +8,15 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const expectedJson = readFileSync(getFixturePath('expectedJson.txt'), 'utf8');
-const pathFile1 = getFixturePath('file1.json');
-const pathFile2 = getFixturePath('file2.json');
+const pathFileJson1 = getFixturePath('file1.json');
+const pathFileJson2 = getFixturePath('file2.json');
 test('Comparison of flat json files', () => {
-  expect(genDiff(pathFile1, pathFile2)).toBe(expectedJson);
+  expect(genDiff(pathFileJson1, pathFileJson2)).toBe(expectedJson);
+});
+
+const expectedYml = readFileSync(getFixturePath('expectedYml.txt'), 'utf8');
+const pathFileYml1 = getFixturePath('file1.yml');
+const pathFileYml2 = getFixturePath('file2.yml');
+test('Comparison of flat yml files', () => {
+  expect(genDiff(pathFileYml1, pathFileYml2)).toBe(expectedYml);
 });
