b11670e0c0eca24e05c2bb2db3eb7fe8e340d49f
Valentino-vada <tataurov_qi@yahoo.com>
2021-08-31 22:19:02 +0300

    changed src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,13 @@
 import stylish from './stylish.js';
+import plain from './plain.js';
 import jsonFormatter from './json.js';
 
 const makeFormatter = (format) => {
   if (format === 'json') {
     return jsonFormatter;
+  }
+  if (format === 'plain') {
+    return plain;
   }
   return stylish;
 };
