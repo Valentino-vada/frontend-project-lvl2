4bfad65448f09b90b52f11633f2d2d39bbc5ec31
Valentino-vada <tataurov_qi@yahoo.com>
2021-08-31 22:16:00 +0300

    added to bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,8 +7,8 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
-  .argument('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, type = program.format) => {
+    console.log(genDiff(filepath1, filepath2, type.format));
   });
 program.parse(process.argv);
