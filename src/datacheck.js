e30675d67d17718b9e90d2d5873150abce24b37e
Valentino-vada <tataurov_qi@yahoo.com>
2021-08-31 22:18:17 +0300

    deleted src/datacheck.js

--- src/datacheck.js
+++
@@ -1,6 +0,0 @@
-export default (obj, key) => {
-  const type = typeof obj[key];
-  const boolObj = obj[key] !== null && (type === 'object' || type === 'function');
-  const boolArr = Array.isArray(obj[key]);
-  return boolObj && !boolArr;
-};
